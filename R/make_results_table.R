97c1029619d2faa9c61cb8b0d9ee073a80ab196b
Frederick Boehm <frederick.boehm@gmail.com>
2018-02-04 11:47:33 -0600

    updated with code for make_results_table2()

--- R/make_results_table.R
+++ R/make_results_table.R
@@ -20,3 +20,25 @@ make_results_table <- function(boot_run_num = 301, total_number_of_scans_per_tra
   do.call("rbind", res) -> tib
   return(tib)
 }
+
+
+#' Read boot output files - which contain lrt statistics - and assemble into a single table, try 2
+#'
+#' @param boot_run_num integer to indicate which run of experimental design this is.
+#' @export
+
+make_results_table2 <- function(boot_run_num = 305){
+  directory <- paste0("results-chtc/boot400-run", boot_run_num)
+  out <- list()
+  i <- 1
+  for (fnpre in dir(directory)){
+    fn <- file.path(directory, fnpre)
+    read.table(fn) -> foo
+    dplyr::mutate(foo, filename = fnpre) -> out[[i]]
+    i <- i + 1
+  }
+  do.call("rbind", out) -> tib
+  return(tib)
+}
+
+
