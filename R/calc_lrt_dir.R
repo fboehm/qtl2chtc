38631fa18ded2198fe8b8cacdc6bf364d51c0166
Frederick Boehm <frederick.boehm@gmail.com>
2018-10-04 11:33:00 -0500

    updated calc_lrt_dir code and doc

--- R/calc_lrt_dir.R
+++ R/calc_lrt_dir.R
@@ -1,11 +1,12 @@
 #' Calculate likelihood ratio tests for all files in a single directory. Each file contains output of a two-dimensional scan.
 #'
-#' @param directory path to directory that contains the collection of files.
+#' @param directory path to directory that contains the collection of files with names ending in the specified pattern.
+#' @param pattern regular expression to pass to dir() to identify the results files
 #' @export
 #' @importFrom utils read.table
 
-calc_lrt_dir <- function(directory){
-  dir(directory) -> fns
+calc_lrt_dir <- function(directory, pattern = "*.txt"){
+  dir(path = directory, pattern = pattern) -> fns
   lrt <- numeric()
   i <- 1
   for (fn in fns){
